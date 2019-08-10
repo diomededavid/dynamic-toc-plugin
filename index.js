7b59071b8f2e369a194e4ca22d2311f1fad9b9c5
diomededavid <david@d2creativedesign.com>
2019-08-10 16:31:31 -0700

    Wrap in a function

--- index.js
+++ index.js
@@ -2,31 +2,32 @@
 import './style.css';
 
 // Table of Contents
-const toc =  document.querySelector('#toc');
-const headings = document.querySelectorAll('h2');
+const tableOfContents = function () {
+  const toc = document.querySelector('#toc');
+  const headings = document.querySelectorAll('h2');
 
-
-/**
- * Get the heading ID or create one if it doesn't
- * @param {Node} elem The getElementById
- * @return {String} The ID
- */
-const getID = function(elem) {
-  // If the element doesn't have an ID, create one
-    if(!elem.id) {
+  /**
+   * Get the heading ID or create one if it doesn't
+   * @param {Node} elem The getElementById
+   * @return {String} The ID
+   */
+  const getID = function (elem) {
+    // If the element doesn't have an ID, create one
+    if (!elem.id) {
       elem.id = elem.textContent.replace(new RegExp(' ', 'g'), '-');
       // Get the elementID 
     }
     return elem.id;
-};
+  };
 
-// Loop through all of the headings and create an array of table of contents items
-const tocItems = Array.prototype.map.call(headings, function(heading){
-  return `<li><a href="#${getID(heading)}">${heading.textContent}</a></li>`
-});
+  // Loop through all of the headings and create an array of table of contents items
+  const tocItems = Array.prototype.map.call(headings, function (heading) {
+    return `<li><a href="#${getID(heading)}">${heading.textContent}</a></li>`
+  });
 
-// Inject table of contents into the DOM
-if(toc && tocItems.length > 0 ) {
-  toc.innerHTML = `<h2>Table of Contents</h2>
+  // Inject table of contents into the DOM
+  if (toc && tocItems.length > 0) {
+    toc.innerHTML = `<h2>Table of Contents</h2>
                     <ul>${tocItems.join('')}</ul>`
-}
+  }
+}();
