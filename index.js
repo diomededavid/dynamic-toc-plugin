07ebe018bcb7424ee3824491b78c4279c5099ea5
diomededavid <david@d2creativedesign.com>
2019-08-11 13:17:27 -0700

    1 file modified

--- index.js
+++ index.js
@@ -2,9 +2,25 @@
 import './style.css';
 
 // Table of Contents
-const tableOfContents = function () {
+const tableOfContents = function(options) {
+
+  // Get the Table of Contents container
   const toc = document.querySelector('#toc');
+  // Get all of the headings
   const headings = document.querySelectorAll('h2');
+
+  // Default Settings
+  const defaults = {
+    selector: 'h2',
+    target: '#toc',
+    addLinks: true,
+    heading: 'Table of Contents'
+  }
+
+  const headings, toc, settings;
+
+  // Merge user options into defaults
+  settings = Object.assign({}, defaults, options);
 
   /**
    * Get the heading ID or create one if it doesn't
@@ -12,7 +28,8 @@ const tableOfContents = function () {
    * @return {String} The ID
    */
   const getID = function (elem) {
-    // If the element doesn't have an ID, create one
+
+    // If the element doesn't have an ID, (or create one if it doesn't have one)
     if (!elem.id) {
       elem.id = elem.textContent.replace(new RegExp(' ', 'g'), '-');
       // Get the elementID 
